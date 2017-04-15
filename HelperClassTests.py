9932c09b574b2d715d2d1c42420e418b279ef21e
Daniel Balberchak <djbalb@gmail.com>
2017-04-15 11:52:58 -0400

    Some initial files to use to track lines and intersection points.  Also a few basic tests.

---
+++ HelperClassTests.py
@@ -0,0 +1,19 @@
+import HelperClasses as hc
+
+
+line1 = hc.LineToIntersect(1,1,1,4)
+line2 = hc.LineToIntersect(1,4,4,4)
+line3 = hc.LineToIntersect(1,1,4,4)
+line4 = hc.LineToIntersect(1,4,4,1)
+line5 = hc.LineToIntersect(1,1,4,1)
+line6 = hc.LineToIntersect(4,1,4,4)
+
+intersectionPoint1 = line1.find_intersection_int(line2) # should be True
+intersectionPoint2 = line3.find_intersection_int(line4) # should be True
+intersectionPoint3 = line2.find_intersection_int(line5) # should be False
+intersectionPoint4 = line6.find_intersection_int(line1) # should be False
+
+print "intersection of line {0} with line {1} calculated to be {2}".format(line1, line2, intersectionPoint1)
+print "intersection of line {0} with line {1} calculated to be {2}".format(line3, line4, intersectionPoint2)
+print "intersection of line {0} with line {1} calculated to be {2}".format(line2, line6, intersectionPoint3)
+print "intersection of line {0} with line {1} calculated to be {2}".format(line5, line1, intersectionPoint4)
---
+++ HelperClasses.py
@@ -0,0 +1,60 @@
+class LineToIntersect:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        
+        # calculate slope on line construction
+        if(x2 <> x1):
+            self.slope = ((y2 - y1)) / float(((x2 - x1)))
+            self.slopeAbs = abs(self.slope)
+        else:
+            # vertical line
+            self.slope = float('inf')
+            self.slopeAbs = float('inf')
+
+    def __str__(self):
+        return "[({0},{1}), ({2},{3})]".format(self.x1, self.y1, self.x2, self.y2)
+
+    
+    # see determinant method here https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+    # Useful for determining line intersection given 4 points (2 on each line).
+    # Assumes parameters of type LineToIntersect.  Performs calculations and returns intersection point
+    # with coordinates.  Note that coordinates may be the result of division that has truncated decimal places.
+    #
+    # Returns IntersectionPoint with valid set to True, or False if no intersection
+    def find_intersection_int(self, otherLine):
+        # print 'self slope = {0}, other slope = {1}'.format(self.slope, otherLine.slope)
+
+        if (self.slope == otherLine.slope):
+            return IntersectionPoint(0,0, False)
+        else:
+            # (x1y2 - y1x2)
+            numeratorFirst = (self.x1 * self.y2 - self.y1 * self.x2) 
+
+            # (x3y4 - y3x4)
+            numeratorSecond = (otherLine.x1 * otherLine.y2 - otherLine.y1 * otherLine.x2)
+
+            #(x1 -x2)(y3 - y4) - (y1 - y2)(x3 - x4)
+            denominator = (self.x1 - self.x2) * (otherLine.y1 - otherLine.y2) - (self.y1 - self.y2) * (otherLine.x1 - otherLine.x2)
+
+            xNumerator = (numeratorFirst) * (otherLine.x1 - otherLine.x2) - (self.x1 - self.x2) * numeratorSecond
+            yNumerator = (numeratorFirst) * (otherLine.y1 - otherLine.y2) - (self.y1 - self.y2) * numeratorSecond
+
+            # note that this will truncate if actual value has decimal places
+            xCoordinate = xNumerator / denominator
+            yCoordinate = yNumerator / denominator
+
+            intersection = IntersectionPoint(xCoordinate, yCoordinate, True)
+            return intersection
+
+
+class IntersectionPoint:
+    def __init__(self, x, y, valid):
+        self.x = x
+        self.y = y
+        self.valid = valid
+
+    def __str__(self):
+        return "({0},{1}): valid = {2}".format(self.x, self.y, self.valid)
