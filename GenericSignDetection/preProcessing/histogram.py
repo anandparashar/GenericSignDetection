05b2ac5d69981df54bf29cb6bcf42ccbbe761e15
Joseph Kinzel <functional.concepts@gmail.com>
2017-04-27 15:51:49 -0400

    Poly detection rework

---
+++ GenericSignDetection/preProcessing/histogram.py
@@ -0,0 +1,124 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Observer
+#
+# Created:     25/04/2017
+# Copyright:   (c) Observer 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import math
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+'''  Estimates edge threshold from image gradient
+
+    img - Source image
+    returns: Scalar threshold value
+
+'''
+def estimateThreshold(img):
+    # Blur image and calculate Sobel gradient magnitude
+    blurred = cv2.GaussianBlur(img, (3, 3), 1.5)
+    grad_x = cv2.Sobel(blurred, cv2.CV_32F, 1, 0)
+    grad_y = cv2.Sobel(blurred, cv2.CV_32F, 0, 1)
+    grad = abs(grad_x) + abs(grad_y)
+
+    # Flatten and sort array
+    gsorted = grad.flatten()
+    gsorted.sort()
+
+    # Threshold value will leave the upper 8% of edge pixels intact
+    threshold =  gsorted[ int(gsorted.size*0.95)]
+    return threshold
+
+''' Calculates a 32 bin histogram from a grayscale image 
+    
+    img - Source image
+    
+    returns: Array representing histogram
+'''
+
+def getGrayscaleHistogram(img):
+    hist = cv2.calcHist([img], [0], None, [32], [0, 256] )
+    return hist
+
+''' Attempts to automatically correct gamma for a source image
+
+    img - Source image
+    returns: Gamma corrected image
+'''
+def gammaCorrect(img):
+    hist = getGrayscaleHistogram(img)
+
+    # Get the maximum value index
+    p1 = np.argmax(hist)
+
+    # Get two sequences to find the next highest peak
+    seq1 = hist[0:p1-6]
+    seq2 = hist[p1+6:]
+
+    # Check for empty sequences due to the the highest peak being too close to a luminance boundary
+    if len(seq1) > 0:
+        c1 = np.argmax(hist[0:p1])
+    else:
+        c1 = p1+1
+    if len(seq2) > 0:
+        c2 = np.argmax(hist[p1+1:]) + p1+1
+
+    # Determine the second highest peak
+    p2 = c1
+    step = -1
+    if hist[c2] >= hist[c1]:
+        p2 = c2
+        step = 1
+
+    diff = p1 - p2
+
+    # Calculate the valley
+    v = np.argmin(hist[p1:p2:step])
+
+    # Make sure the next highest peak is significant enough to care about
+    if hist[p1]/hist[p2] < 4:
+        # Gamma correct and merge
+
+        s1 = sum(hist[0:v])
+        s2 = sum(hist[v+1:])
+
+        masked1 = np.copy(img)
+        masked2 = np.copy(img)
+
+        masked1 = np.ma.masked_less_equal(masked1, v*8)
+        masked2 = np.ma.masked_greater(masked2, v*8)
+
+
+        mean1 = masked1.mean()
+        mean2 = masked2.mean()
+
+        ratio1 = mean1/(v*8)
+        ratio2 = mean2/(v*8)
+
+        l1 = math.log(ratio1, 2)
+        l2 = math.log(ratio2, 2)
+
+        gamma1 = 1.0/l1
+        gamma2 = 1.0 - l2
+
+        gamma = (gamma2 if (255.0 - mean2) < mean1 else gamma2)
+
+        print gamma
+        print gamma1, gamma2
+
+        simple = (img*(1.0/255.0)) ** (1.0/gamma)
+        simple = (simple*255).astype(np.uint8)
+
+
+        return simple
+    else:
+        return img
+
+
+
---
+++ GenericSignDetection/test2.py
@@ -0,0 +1,89 @@
+import numpy as np
+import cv2
+import preProcessing.preProcessor
+import preProcessing.histogram
+import polyDetect.getAllPoly as gp
+import polyDetect.polyshape
+
+
+#Function to display re-sized image
+def displayResized(message, imageDisp):
+    print ("Displaying "+message)
+    height = imageDisp.shape[0]
+    width = imageDisp.shape[1]
+    if width>800 or height >600:
+        aspectratio = float(width) / float(height)
+        width = 800
+        height = width / aspectratio
+    cv2.imshow(message, cv2.resize(imageDisp, (width, int(height))))
+
+
+def main():
+    #read image
+
+    img= cv2.imread("/home/joseph/cs682/final/Data/aiua120306-1/"
+                    "frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveLeft_1333396989.avi_image1.png")
+    # "frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveRight_1333396823.avi_image4.png
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    vmask = np.tile( [[0, 0], [1, 0]], (gray.shape[0]/2, gray.shape[1]/2))
+
+    noise = np.ma.array(gray, mask=vmask)
+
+    filterKern = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])*(1.0/8.0)
+
+    mfiltered = cv2.filter2D(gray, 0, filterKern).astype(np.uint8)
+
+    mmasked = np.ma.array(mfiltered, mask=np.logical_not(vmask))
+
+    cleaned = mmasked.filled(0) + noise.filled(0)
+
+    cv2.imshow('Cleaned', cleaned)
+    cv2.imwrite('/home/joseph/cleaned.png', cleaned)
+
+
+
+    fixed = preProcessing.histogram.gammaCorrect(cleaned)
+    fixedequal = cv2.equalizeHist(fixed)
+
+
+    fixedFull = cv2.cvtColor(fixed, cv2.COLOR_GRAY2BGR)
+
+
+    threshold = preProcessing.histogram.estimateThreshold(fixedFull)
+    processed, lines = preProcessing.preProcessor.process(image=fixedFull, canny_param1=threshold / 2,
+                                       canny_param2=threshold,
+                                       harriscorner_blockSize=2,
+                                       harriscorner_kSize=3,
+                                       harriscorner_freeparam=0.4,
+                                       smallsegmentremoval_ratio=0.05,
+                                       hough_threshold=10,
+                                       hough_minLen=15,
+                                       hough_maxGap=4
+                                       )
+
+    bigKern = np.ones( (2,2), dtype=np.uint8)
+
+    bigger = cv2.dilate(processed, bigKern)
+
+
+
+    shapes = gp.getAllPoly(bigger, 100, 1000)
+
+    imshape = img.shape
+
+    outImg = np.copy(fixedFull)
+
+    cv2.imshow('Final', processed)
+
+    for s in shapes:
+        s.drawBoundingRect(outImg, (0, 255, 0))
+        s.drawContour(outImg, (255, 0, 0))
+
+    cv2.imshow('Shapes', outImg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+main()
