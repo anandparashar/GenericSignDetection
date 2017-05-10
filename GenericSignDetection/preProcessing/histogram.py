b5183ae6d6af96b839bec72ecf918dc2a31be8a9
Observer <Observer>
2017-05-09 22:57:46 -0400

    Final testing commit

--- GenericSignDetection/preProcessing/histogram.py
+++ GenericSignDetection/preProcessing/histogram.py
@@ -1,18 +1,7 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Observer
-#
-# Created:     25/04/2017
-# Copyright:   (c) Observer 2017
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 import math
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import scipy.stats as stats
 
 '''  Estimates edge threshold from image gradient
 
@@ -25,14 +14,20 @@ def estimateThreshold(img):
     blurred = cv2.GaussianBlur(img, (3, 3), 1.5)
     grad_x = cv2.Sobel(blurred, cv2.CV_32F, 1, 0)
     grad_y = cv2.Sobel(blurred, cv2.CV_32F, 0, 1)
-    grad = abs(grad_x) + abs(grad_y)
+
+    if  (len(img.shape) > 2):
+        grad_d = abs(grad_x)**2 + abs(grad_y)**2
+        grad_d = np.sum(grad_d, axis=2)
+        grad_d = np.sqrt(grad_d)
+    else:
+        grad_d = abs(grad_x ) + abs(grad_y)
 
     # Flatten and sort array
-    gsorted = grad.flatten()
+    gsorted = grad_d.flatten()
     gsorted.sort()
 
-    # Threshold value will leave the upper 8% of edge pixels intact
-    threshold =  gsorted[ int(gsorted.size*0.96)]
+    # Threshold value will leave the upper 10% of edge pixels intact
+    threshold = gsorted[ int(gsorted.size*0.94)]
     return threshold
 
 ''' Calculates a 32 bin histogram from a grayscale image 
@@ -42,96 +37,95 @@ def estimateThreshold(img):
     returns: Array representing histogram
 '''
 
-def getGrayscaleHistogram(img):
-    hist = cv2.calcHist([img], [0], None, [32], [0, 256] )
-    return hist
-
-''' Attempts to automatically correct gamma for a source image
-
-    img - Source image
-    returns: Gamma corrected image
 '''
-def gammaCorrect(img):
-    hist = getGrayscaleHistogram(img)
+    Attempts to perform contrast and luminance correction on an image via an adaptive MSRCR algorithm
+    
+    img - Source image to correct
+    kernSize - Size of the Gaussian kernel for the MSRCR reflectance calculation
+    big_k - Kernel scalar
+    little_k - Essentially an overall luminance scalar, keep it to values 0.9 and below
+    dr_thresh - Dark region threshold, value between 0 and 1 representing the upper limit of dark pixels
+                required to trigger contrast correction
+                 
+    Returns - A correct image if dr_tresh is breached or the original image if it is not
+'''
+def contrastCorrect(img, kernSize, big_k, little_k, dr_thresh):
 
-    # Get the maximum value index
-    p1 = np.argmax(hist)
+    yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb).astype(np.float64)
 
-    # Get two sequences to find the next highest peak
-    seq1 = hist[0:p1-6]
-    seq2 = hist[p1+6:]
+    # Extract component channels
+    lum, cb, cr = cv2.split(yuv)
 
-    # Check for empty sequences due to the the highest peak being too close to a luminance boundary
-    if len(seq1) > 0:
-        c1 = np.argmax(seq1)
-    else:
-        c1 = -1
+    # Make sure there are no zero values with the luminance to avoid a divide by zero condition
+    lum = np.where(lum > 0, lum, np.ones(lum.shape, dtype=np.float64))
 
-    if len(seq2) > 0:
-        c2 = np.argmax(seq2) + p1+6
-    else:
-        c2 = -1
+    # Count the number of significantly dark pixels
+    dark_count = len(lum[lum < 48])
 
-    if c2 == c1:
-        return img
-    elif c1 == -1:
-        p2 = c2
-        step = 1
-    elif c2 == -1:
-        p2 = c1
-        step = -1
-    elif hist[c2] >= hist[c1]:
-        p2 = c2
-        step = 1
-    else:
-        p2 = c1
-        step = -1
+    dark_ratio = float(dark_count)/lum.size
 
-    diff = p1 - p2
+    # Calculate the skewness of the luminance channel
+    skewness = stats.skew(lum, axis=None, bias=False)
 
-    # Calculate the valley
-    v = np.argmin(hist[p1:p2:step])
+    # Only apply if there's significant enough shade in the image
 
-    print p1, p2
-
-    # Make sure the next highest peak is significant enough to care about
-    if hist[p1]/hist[p2] < 4:
-        # Gamma correct and merge
-
-        s1 = sum(hist[0:v])
-        s2 = sum(hist[v+1:])
-
-        masked1 = np.copy(img)
-        masked2 = np.copy(img)
-
-        masked1 = np.ma.masked_less_equal(masked1, v*8)
-        masked2 = np.ma.masked_greater(masked2, v*8)
-
-
-        mean1 = masked1.mean()
-        mean2 = masked2.mean()
-
-        ratio1 = mean1/(v*8)
-        ratio2 = mean2/(v*8)
-
-        l1 = math.log(ratio1, 2)
-        l2 = math.log(ratio2, 2)
-
-        gamma1 = 1.0/l1
-        gamma2 = 1.0 - l2
-
-        gamma = (gamma2 if (255.0 - mean2) < mean1 else gamma2)
-
-        print gamma
-        print gamma1, gamma2
-
-        simple = (img*(1.0/255.0)) ** (1.0/gamma)
-        simple = (simple*255).astype(np.uint8)
-
-
-        return simple
-    else:
+    if dark_ratio < dr_thresh:
         return img
 
 
+    # C values and weights for gaussian filters
+    cvals = [20, 80, 250]
+    weights = [1.0/len(cvals) for i in range(0, len(cvals))]
+
+    kerns = []
+
+    # Build gaussian filters
+    for c in cvals:
+        kern = np.empty((kernSize, kernSize), dtype=np.float64)
+        for y in range(0, kernSize):
+            for x in range(0, kernSize):
+                a_x = float(abs((kernSize-1)/2 + 1 - x))
+                a_y = float(abs((kernSize-1)/2 + 1 - y))
+                kern[y, x] = big_k*math.exp(-1*(float(a_x**2 + a_y**2)/(c**2)) )
+
+        kern_sum = np.sum(kern)
+        kerns.append(kern)
+
+    reflectance = np.zeros(lum.shape, dtype=np.float64)
+
+    # Calculate reflectance
+    lum_log = np.log10(lum)
+
+    for i in range(0, len(weights)):
+        # filtered = cv2.GaussianBlur(lum, (kernSize, kernSize), cvals[i], cvals[i])
+        filtered = cv2.filter2D(lum, cv2.CV_64F, kerns[i])
+        reflectance += weights[i]*(lum_log - np.log10(filtered) )
+
+    # Calculate mu
+    mu = ((-1*skewness)/2.0 + little_k)
+
+    # Calculate beta
+    rmin = np.min(reflectance)
+    rmax = np.max(reflectance)
+
+    beta = ((reflectance - rmin)/(rmax - rmin))*mu
+
+    # Calculate new luminance channel values
+    lum_prime = (beta*(256 - 1))
+
+    # Calculate chroma adjustment scalar
+    cdeg = lum_prime/lum
+
+    # Calculate new chroma values
+    cb_prime = ((cdeg*(cb.astype(np.float64) - 128)) + 128)
+    cr_prime = ((cdeg*(cr.astype(np.float64) - 128)) + 128)
+
+    # Pack channels back together
+    yuv_prime = cv2.merge( (lum_prime, cb_prime, cr_prime))
+    yuv_prime = yuv_prime.astype(np.uint8)
+
+    # Convert back to BGR
+    bgr = cv2.cvtColor(yuv_prime.astype(np.uint8), cv2.COLOR_YCrCb2BGR)
+
+    return bgr
 
--- GenericSignDetection/test2.py
+++ GenericSignDetection/test2.py
@@ -8,6 +8,7 @@ import polyDetect.polyshape
 import polyDetect.cycleDetection as cd
 from datetime import datetime
 import ntpath
+import math
 
 
 #Function to display re-sized image
@@ -21,55 +22,132 @@ def displayResized(message, imageDisp):
         height = width / aspectratio
     cv2.imshow(message, cv2.resize(imageDisp, (width, int(height))))
 
+def colorEdgeDetect(img, cthresh1, cthresh2):
+
+    grads = []
+    edges = []
+    y, u, v = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2YUV))
+
+    uv_mag, uv_angles = cv2.cartToPolar(u.astype(np.float32), v.astype(np.float32), angleInDegrees=True)
+
+    edge_channels = [y, uv_angles]
+
+    yf = y.astype(np.float32)
+
+    sig = (uv_angles%90 - 45)
+
+    sig = np.abs(sig) - 15
+    sig /= sig.max()
+    sig *= 255
+
+    # sig_gaussian = cv2.GaussianBlur(sig, (3, 3), 1.25)
+    ang_gaussian = cv2.GaussianBlur(uv_angles, (3, 3), 1.25)
+
+    sig_gaussian = sig
+
+    ang_gx = cv2.Sobel(ang_gaussian, cv2.CV_32F, 1, 0)
+    ang_gy = cv2.Sobel(ang_gaussian, cv2.CV_32F, 0, 1)
+
+    sig_gx = cv2.Sobel(sig_gaussian, cv2.CV_32F, 1, 0)
+    sig_gy = cv2.Sobel(sig_gaussian, cv2.CV_32F, 0, 1)
+
+    ang_mag = cv2.magnitude(ang_gx, ang_gy)
+    sig_mag = cv2.magnitude(sig_gx, sig_gy)
+
+    ang_mag /= ang_mag.max()
+
+    sig_mag /= sig_mag.max()
+
+    combined = np.round(ang_mag * sig_mag * 255)
+
+    sorted = np.sort(combined.copy().flatten())
+
+    threshold = sorted[ int(sorted.size*0.99) ]
+
+    edge_guess = cv2.Canny(combined.astype(np.uint8), threshold, threshold/3)
+    edge_guess = cv2.morphologyEx(edge_guess, cv2.MORPH_CLOSE, np.ones((2, 2)))
+
+    cv2.imshow('Edges from color', edge_guess)
+
+    e2 = pp.skeltonize(edge_guess)
+    e2gray = e2
+    normCanny = cv2.Canny(img, cthresh1, cthresh2)
+
+    edgeImg = cv2.bitwise_or( normCanny, e2gray )
+
+    cv2.imshow('CE rough edges', edgeImg)
+
+
+
+
+    cv2.imshow('Significant matrix', combined.astype(np.uint8)*8)
+    cv2.waitKey(0)
+
+
+    return edgeImg
+
+
+
+
 
 def main():
 
     print cv2.__version__
     #read image
+    # imageFileName = "C:/Code/cs682/final/Data/vid8/frameAnnotations-MVI_0120.MOV_annotations/speedLimit_1324866418.avi_image7.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120306-1/frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveRight_1333396823.avi_image4.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120306-1/frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveRight_1333395619.avi_image10.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120306-1/frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/pedestrianCrossing_1333395580.avi_image17.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120306-1/frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/dip_1333396129.avi_image7.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120214-2/frameAnnotations-DataLog02142012_002_external_camera.avi_annotations/curveRight_1331866657.avi_image18.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120214-2/frameAnnotations-DataLog02142012_002_external_camera.avi_annotations/laneEnds_1331866559.avi_image7.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120214-2/frameAnnotations-DataLog02142012_002_external_camera.avi_annotations/merge_1331866392.avi_image6.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120214-2/frameAnnotations-DataLog02142012_002_external_camera.avi_annotations/merge_1331866676.avi_image10.png"
+    # imageFileName = "C:/Code/cs682/final/Data/aiua120214-2/frameAnnotations-DataLog02142012_002_external_camera.avi_annotations/merge_1331866749.avi_image1.png"
 
-    imageFileName = "D:/Study/CS-682ComputerVision/LISATrafficSignDatabase/signDatabasePublicFramesOnly/aiua120306-1/frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveRight_1333396823.avi_image4.png"
-    img= cv2.imread(imageFileName)
+    # imageFileName = "C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/speedLimit_1324866741.avi_image5.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid8/frameAnnotations-MVI_0120.MOV_annotations/keepRight_1324866323.avi_image1.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/pedestrianCrossing_1324866507.avi_image0.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/speedLimit_1324866786.avi_image4.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/speedLimit_1324866741.avi_image3.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid8/frameAnnotations-MVI_0120.MOV_annotations/keepRight_1324866323.avi_image5.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid8/frameAnnotations-MVI_0120.MOV_annotations/pedestrianCrossing_1324866135.avi_image5.png"
+    imageFileName = "C:/Code/cs682/final/Data/vid8/frameAnnotations-MVI_0120.MOV_annotations/speedLimit_1324866175.avi_image4.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid10/frameAnnotations-MVI_0122.MOV_annotations/signalAhead_1324866992.avi_image17.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid11/frameAnnotations-MVI_0123.MOV_annotations/pedestrianCrossing_1324867104.avi_image2.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid11/frameAnnotations-MVI_0123.MOV_annotations/signalAhead_1324867127.avi_image15.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid11/frameAnnotations-MVI_0123.MOV_annotations/speedLimit_1324867178.avi_image5.png"
+    # imageFileName = "C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/pedestrianCrossing_1324866507.avi_image7.png"
 
-    # img = cv2.imread("C:/Code/cs682/final/Data/vid8/"
-    #                 "frameAnnotations-MVI_0120.MOV_annotations/speedLimit_1324866418.avi_image7.png")
+    img = cv2.imread(imageFileName)
 
-    #img = cv2.imread("C:/Code/cs682/final/Data/aiua120306-1/"
-    # "frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/curveRight_1333395619.avi_image10.png")
 
-    #img = cv2.imread("C:/Code/cs682/final/Data/aiua120306-1/"
-    # "frameAnnotations-DataLog02142012_003_external_camera.avi_annotations/dip_1333396129.avi_image7.png")
+    #img = cv2.imread()
 
-    # img = cv2.imread("C:/Code/cs682/final/Data/vid9/frameAnnotations-MVI_0121.MOV_annotations/speedLimit_1324866741.avi_image5.png")
+    # img = cv2.imread()
+    cv2.imshow('Original', img)
 
-    # Get rid of the non-image junk at the bottom of the aiua* direct images
-    trimmed = np.delete(img, range(img.shape[0] - 16, img.shape[0]), axis=0)
+    # Screen for images that need additional cleanup and apply it if necessary
+    needsCleanup = "aiua12" in imageFileName
 
-    # Convert to grayscale if the image is in color
-    if img.shape[2] == 3:
-        gray = cv2.cvtColor(trimmed, cv2.COLOR_BGR2GRAY)
+    if needsCleanup:
+
+        # Get rid of the non-image junk at the bottom of the aiua* direct images
+        trimmed = np.delete(img, range(img.shape[0] - 16, img.shape[0]), axis=0)
+        # Remove screen effect
+        cleaned = ilr.clean(trimmed)
+
+        fixedFull = cv2.cvtColor(cleaned, cv2.COLOR_GRAY2BGR)
+        # Fix contrast if necessary
+        fixedFull = hist.contrastCorrect(fixedFull, 151, 1, 0.8, 0.25)
     else:
-        gray = trimmed
-
-    # Clean interlacing, only necessary in the aiua* data folders
-    cleaned = ilr.clean(trimmed)
-
-    cv2.imshow('Cleaned', cleaned)
-    cv2.waitKey(0)
-    # cv2.imwrite('/home/joseph/cleaned.png', cleaned)
-
-    fixed = cleaned
-    # fixed = hist.gammaCorrect(cleaned)
-
-
-    # Back to full color
-    fixedFull = cv2.cvtColor(fixed, cv2.COLOR_GRAY2BGR)
-    displayResized("fixed full", fixedFull)
-    # cv2.waitKey(0)
+        fixedFull = hist.contrastCorrect(img, 151, 3, 0.8, 0.25)
 
     # Estimate a threshold value for the edge detector
     threshold = hist.estimateThreshold(fixedFull)
 
-    processed, p2, lines = pp.process(image=fixedFull, canny_param1=threshold /2,
+    # Run the rest of the preprocessing
+    processed, p2, lines = pp.process(image=fixedFull, canny_param1=threshold /3,
                                        canny_param2=threshold,
                                        harriscorner_blockSize=2,
                                        harriscorner_kSize=3,
@@ -80,38 +158,12 @@ def main():
                                        hough_maxGap=7
                                        )
 
-    '''
-    lineImg = np.copy(fixedFull)
-    for (p1, p2) in lines:
-        cv2.line(lineImg, p1, p2, color=(0, 0, 255), thickness=1)
-    cv2.imshow('Lines', lineImg)
-    '''
-
-    cv2.imshow('Final', processed)
-
-    cv2.waitKey(0)
-
-    '''
-    bigKern = np.ones( (2,2), dtype=np.uint8)
-
-    bigger = cv2.dilate(processed, bigKern)
-
-    lineImg = np.copy(fixedFull)
-
-    for (p1, p2) in lines:
-        cv2.line(lineImg, p1, p2, (0, 0, 255), thickness=1)
-
-    cv2.imshow('Processed', processed)
-    cv2.imshow('Lines', lineImg)
-    cv2.waitKey(0)
-
-    '''
 
     # Stuff for processing time management
     polystart = datetime.now()
 
     # Workhorse function for polygon detection
-    shapes = gp.getAllPoly(processed, 169, 12000, 5, 8, 4)
+    shapes = gp.getAllPoly(processed, 169, 12000, 7, 15, 5)
 
     imshape = img.shape
 
@@ -120,12 +172,15 @@ def main():
     # Filter for regular shapes
     goodShapes = [s for s in shapes if s.isGoodSignCandidate(12)]
 
+    print 'Found ' + str(len(goodShapes)) + ' candidate shapes'
+
     bestMask = [True for i in range(0, len(goodShapes)) ]
 
     # Eliminate overlapping shapes
     for i in range(0, len(goodShapes)):
         for j in range(i+1, len(goodShapes)):
-            if goodShapes[i].overlaps(goodShapes[j]):
+            goodShapes[i].drawBoundingRect(outImg, (0, 255, 0))
+            if goodShapes[i].boundingIoU(goodShapes[j]) > 0.5:
                 if goodShapes[i].area > goodShapes[j].area:
                     bestMask[j] = False
                 else:
@@ -139,6 +194,7 @@ def main():
     for bs in bestShapes:
         color = (0, 0, 255)
         bs.drawContour(outImg, color)
+        bs.drawBoundingRect(outImg, (0, 255, 0) )
 
     print "Found " + str( len(bestShapes) ) + " potential signs in " + str((polyend - polystart).microseconds/1000) + 'ms'
 
